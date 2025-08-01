14e06d6cd020329ec63c469ad61a6dea097e07d9
Guilhermepimentel <145464569+guilherme-pimentel@users.noreply.github.com>
2025-07-31 22:55:36 -0300

    Delete sistema_escolar directory

--- sistema_escolar/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SistemaEscolarConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sistema_escolar'
--- sistema_escolar/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # página principal do app
-]
