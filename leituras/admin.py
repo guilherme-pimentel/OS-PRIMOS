b31b3ef3c0b08504c235cc6ab807923a76cdfe8c
Guilhermepimentel <145464569+guilherme-pimentel@users.noreply.github.com>
2025-09-12 18:04:31 -0300

    Delete leituras directory

--- leituras/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Leitura
-
-admin.site.register(Leitura)
-    
--- leituras/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LeiturasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'leituras'
