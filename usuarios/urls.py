09d134fef66eec9377622774cb7ed4c890de648c
Guilhermepimentel <145464569+guilherme-pimentel@users.noreply.github.com>
2025-09-12 18:04:12 -0300

    Delete usuarios directory

--- usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_usuarios, name='lista_usuarios'),
-    path('adicionar/', views.adicionar_usuario, name='adicionar_usuario'),
-    path('editar/<int:id>/', views.editar_usuario, name='editar_usuario'),
-    path('excluir/<int:id>/', views.excluir_usuario, name='excluir_usuario'),
-]
