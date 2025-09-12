bcfc3a6822adea3c5b3a66dac8cc79b02b4bac6b
Guilhermepimentel <145464569+guilherme-pimentel@users.noreply.github.com>
2025-09-12 18:04:39 -0300

    Delete faturas directory

--- faturas/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Fatura
-
-admin.site.register(Fatura)
--- faturas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_faturas, name='lista_faturas'),
-    path('adicionar/', views.adicionar_fatura, name='adicionar_fatura'),
-    path('editar/<int:id>/', views.editar_fatura, name='editar_fatura'),
-    path('excluir/<int:id>/', views.excluir_fatura, name='excluir_fatura'),
-]
